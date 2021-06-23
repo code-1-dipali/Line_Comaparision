af9471b9184aeb7d22e69a9836fc93e5a90b47b7
ikalpesh <kalpeshmahajan90@gmail.com>
2021-06-23 19:11:28 +0530

    Check_Utality_Of_2_Length

--- LineComparision.java
+++ LineComparision.java
@@ -4,24 +4,27 @@ import java.util.*;
 public class Line {
 	Point p1, p2;
 	
+	@Override
+	public int hashCode() {
+		return Objects.hash(p1, p2);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		/*if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Line other = (Line) obj;
+		return Objects.equals(p1, other.p1) && Objects.equals(p2, other.p2);
+		*/
+		
+		return this.getLength() == ((Line)obj).getLength();
+	}
+
 	public double getLength() {
 		return Math.sqrt(((p2.x-p1.x)*(p2.x-p1.x))+((p2.y-p1.y)*(p2.y-p1.y)));
-	}
-	
-	public static void main(String args[]) {
-		
-		Point p1 = new Point();
-		p1.x = 4;
-		p1.y = 3;
-		
-		Point p2 = new Point();
-		p2.x = 6;
-		p2.y = 3;
-		
-		Line line1 = new Line();
-		line1.p1 = p1;
-		line1.p2 = p2;
-		
-		System.out.println("Length of line = "+line1.getLength());
 	}
 }
